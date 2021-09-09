85cf46db43f5c385096fd994746483d61a1d36b7
Jochem van Iterson <jochem@audioware.nl>
2021-09-09 15:59:44 +0200

    Convert to function

--- CSD2a/python_basics/playsound.py
+++ CSD2a/python_basics/playsound.py
@@ -1,13 +1,18 @@
+#!/usr/bin/python
 # Example from https://simpleaudio.readthedocs.io/en/latest/
 
 # Import simpleaudio library
 import simpleaudio as sa
 
 # Load the audio from a file, and insert into 'wav_obj'
-
 # NOTE: Can't play 32float, so had to convert original file to 44k 24bit
 wave_obj = sa.WaveObject.from_wave_file("kick.wav")
-# Play the audio file
-play_obj = wave_obj.play()
-# Wait until it's done playing
-play_obj.wait_done()
+
+def playAudioFile():
+  # Play the audio file
+  play_obj = wave_obj.play()
+  # Wait until it's done playing
+  play_obj.wait_done()
+
+
+playAudioFile()
