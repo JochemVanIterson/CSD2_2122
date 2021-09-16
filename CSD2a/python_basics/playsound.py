3f6b918ea35fb1a8f611db72b8edba34672d9f3a
Jochem van Iterson <jochem.vaniterson@audioware.nl>
2021-09-16 21:35:35 +0200

    change code to use new folder

--- CSD2a/python_basics/playsound.py
+++ CSD2a/python_basics/playsound.py
@@ -6,7 +6,7 @@ import simpleaudio as sa
 
 # Load the audio from a file, and insert into 'wav_obj'
 # NOTE: Can't play 32float, so had to convert original file to 44k 24bit
-wave_obj = sa.WaveObject.from_wave_file("kick.wav")
+wave_obj = sa.WaveObject.from_wave_file("../assets/kick.wav")
 
 def playAudioFile():
   # Play the audio file
--- CSD2a/ritische_playback/ritmischeplayback.py
+++ CSD2a/ritische_playback/ritmischeplayback.py
@@ -8,7 +8,7 @@ import time
 
 # Load the audio from a file, and insert into 'wav_obj'
 # NOTE: Can't play 32float, so had to convert original file to 44k 24bit
-wave_obj = sa.WaveObject.from_wave_file("kick.wav")
+wave_obj = sa.WaveObject.from_wave_file("../assets/kick.wav")
 
 #### Functions
 
